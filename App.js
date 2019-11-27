5470e7c4c16f877c140ff1bdcd6c49e6cb338347
aymericbelliot <aymericbelliot@MacBook-de-Aymeric.local>
2019-11-27 17:37:03 -0400

    C'est bon !

--- App.js
+++ App.js
@@ -1,25 +1,47 @@
+const defaultInput = "\
+# Bienvenue dans mon interpréteur GitHub\n\
+___\n\
+## Voici un titre...\n\
+### ...Et un sous-titre par exemple\n\
+___\n\
+Voici un exemple de code `<div></div>` entre 2 apostrophes.\n\
+```\n\
+// Et un exemple de block de code\n\n\
+<div>\n\
+  <p>Hello World</p>\n\
+</div>\n\
+```\n\n\
+Vous pouvez mettre du texte en **gras**, en _italic_ ou les **_deux_**\n\n\
+Vous pouvez aussi [insérer un lien](#)\n\
+> ou une citation\n\
+Voici un\n\
+- exemple\n\
+- de\n\
+- liste\n\n\
+![React Logo w/ Text](https://goo.gl/Umyytc)\
+";
+
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: "",
-      output: ""
+      input: defaultInput,
+      output: marked(defaultInput).replace(RegExp(/\n/g), '<br>')
     }
   }
 
   changeInput(event) {
     this.setState({
       input: event.target.value,
-      output: marked(event.target.value)
+      output: marked(event.target.value).replace('\n', '<br>')
     });
-    document.getElementById("preview").innerHTML = this.state.output;
+    console.log(this.state.output);
   }
-
-
 
   render() {
     return (
-      <div className="App container-fluid">
+      <div className="App container-fluid bg-light">
         <Editor change={this.changeInput.bind(this)} input={this.state.input} />
         <Previewer output={this.state.output} />
       </div>
@@ -28,36 +50,34 @@ class App extends React.Component {
 }
 
 class Editor extends React.Component {
-
   handleChange() {
     this.props.change(event);
   }
 
   render() {
     return (
-      <div className="card w-50 mt-5">
-        <div className="card-header d-flex justify-content-between align-items-center">
+      <div className="card w-50 mt-5 shadow bg-primary text-white">
+        <div className="card-header d-flex justify-content-between align-items-center shadow">
           Editor<i className="fas fa-expand-arrows-alt"></i>
         </div>
-        <textarea id="editor" className="card-body" onChange={this.handleChange.bind(this)} value={this.props.intput}></textarea>
+        <textarea id="editor" className="card-body" rows="10" onChange={this.handleChange.bind(this)} defaultValue={this.props.input}></textarea>
       </div>
     );
   }
 }
 
 class Previewer extends React.Component {
-
   render() {
     return (
-      <div className="card w-75 mt-5">
-        <div className="card-header d-flex justify-content-between align-items-center">
+      <div className="card w-75 mt-5 mb-5 shadow bg-success text-white">
+        <div className="card-header d-flex justify-content-between align-items-center shadow">
           Previewer<i className="fas fa-expand-arrows-alt"></i>
         </div>
-        <object id="preview" type="type/html" className="card-body"></object>
+        <div id="preview" className="card-body bg-white text-dark" dangerouslySetInnerHTML={{ __html: this.props.output }} />
       </div >
     );
   }
 }
 
-// Rend Application in html
+// Render Application in html page
 ReactDOM.render(<App />, document.querySelector("#root"));
